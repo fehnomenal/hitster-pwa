ef912cfb8cba965d742392e23a3a41df990b5497
Andreas Fehn <fehnomenal@fehn.systems>
2025-05-19 10:58:08 +0200

    fix: add page meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
   const { children }: LayoutProps = $props()
 </script>
 
+<svelte:head>
+  <title>HITSTER PWA</title>
+
+  <meta
+    name='description'
+    content='Play HITSTER in the browser and with YouTube!' />
+</svelte:head>
+
 <div class='p-3'>
   <h1 class='grid font-bold grid-cols-[auto_min-content] mb-12 mt-8'>
     <span class='text-5xl justify-self-center'>HITSTER</span>
